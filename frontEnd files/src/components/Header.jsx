e9e607779aa2a412849ddd6be7f7b671e097a874
gandlaarunDev <gandlaarunsai.dev@gmail.com>
2024-02-24 15:03:41 +0530

    Update App and Added Header Component

---
+++ frontEnd files/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Header.css';
+
+export default function Header(){
+    return (
+        // <h1>Doc Assist App.</h1>
+        <div className="Header">
+            <h1>Sri SaiNath Homeo Clinic</h1>
+        </div>
+    );
+}
