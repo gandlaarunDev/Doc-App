4decacbf3d7839e54bb664a16ad5e7f2939092ee
gandlaarunDev <gandlaarunsai.dev@gmail.com>
2024-02-25 15:46:52 +0530

    Develop Header component

--- frontEnd files/src/components/Header.jsx
+++ frontEnd files/src/components/Header.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import './Header.css';
+import profile from '../Resources/Icons/profile-user2.svg';
 
 export default function Header(){
     return (
         // <h1>Doc Assist App.</h1>
         <div className="Header">
+            <div className="flex_header">
+                <img src={require('../Resources/images/logo.png')} className="logo" alt="Logo"/>
+                <img src={profile} className="profile" alt="Menu"/>
+            </div>
+            
             <h1>Sri SaiNath Homeo Clinic</h1>
         </div>
     );
